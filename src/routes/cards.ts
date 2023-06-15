264acc522ad6a96c8942d7e60a7a017f786816b0
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-06-15 17:02:25 +0300

    feat: Добавить возможность удаления карточки, а так же установки и снятия лайков

--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
-import { getCards, createCard } from "../controllers/cards";
+import {
+  getCards,
+  createCard,
+  deleteCardById,
+  likeCardById,
+  dislikeCardById,
+} from "../controllers/cards";
 
 const router = Router();
 
 router.get("/", getCards);
 router.post("/", createCard);
-router.delete("/:cardId");
-router.put("/:cardId/likes");
-router.delete("/:cardId/likes");
+router.delete("/:cardId", deleteCardById);
+router.put("/:cardId/likes", likeCardById);
+router.delete("/:cardId/likes", dislikeCardById);
 
 export default router;
