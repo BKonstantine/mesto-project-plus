fc7ef7266fb8ada02386d2eb2284c58411f5e6de
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-06-15 19:37:09 +0300

    feat: добавить обработку ошибок user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,46 +1,61 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { CustomRequest } from "types/types";
 import userModel from "../models/user";
+import NotFoundError from "../errors/not-found-error";
 
-export const getUsers = async (req: Request, res: Response) => {
-  try {
-    const users = await userModel.find();
-    res.send(users);
-  } catch (error) {}
+export const getUsers = (req: Request, res: Response, next: NextFunction) => {
+  userModel
+    .find()
+    .then((users) => res.send(users))
+    .catch(next);
 };
 
-export const getUserById = async (req: Request, res: Response) => {
+export const getUserById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const { userId } = req.params;
     const user = await userModel.findById(userId);
     res.send(user);
-  } catch (error) {}
+  } catch (error) {
+    return next(new NotFoundError("Пользователь не найден"));
+  }
 };
 
-export const createUser = (req: Request, res: Response) => {
+export const createUser = (req: Request, res: Response, next: NextFunction) => {
   const { name, about, avatar } = req.body;
   userModel
     .create({ name, about, avatar })
-    .then((user) => res.send({ data: user }));
+    .then((user) => res.send({ data: user }))
+    .catch(next);
 };
 
-export const updateCurrentUser = async (req: CustomRequest, res: Response) => {
+export const updateCurrentUser = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
   const { name, about } = req.body;
 
   const userId = req.user?._id;
   userModel
     .findByIdAndUpdate(userId, { name, about }, { new: true })
-    .then((user) => res.send({ data: user }));
+    .then((user) => res.send({ data: user }))
+    .catch(next);
 };
 
 export const updateAvatarCurrentUser = async (
   req: CustomRequest,
-  res: Response
+  res: Response,
+  next: NextFunction
 ) => {
   const { avatar } = req.body;
 
   const userId = req.user?._id;
   userModel
     .findByIdAndUpdate(userId, { avatar }, { new: true })
-    .then((user) => res.send({ data: user }));
+    .then((user) => res.send({ data: user }))
+    .catch(next);
 };
