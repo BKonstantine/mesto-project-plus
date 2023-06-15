1600c6c4c4acc1e920f640d0063b113ec89a43ef
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-06-15 23:46:39 +0300

    fix: Исправить функцию createUser

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,7 +1,9 @@
 import { Request, Response, NextFunction } from "express";
+import { Error } from "mongoose";
 import { CustomRequest } from "../types/types";
 import userModel from "../models/user";
 import NotFoundError from "../errors/not-found-error";
+import IncorrectDataError from "../errors/incorrect-data-error";
 
 export const getUsers = (req: Request, res: Response, next: NextFunction) => {
   userModel
@@ -28,8 +30,14 @@ export const createUser = (req: Request, res: Response, next: NextFunction) => {
   const { name, about, avatar } = req.body;
   userModel
     .create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch(next);
+    .then((user) => res.status(200).send({ data: user }))
+    .catch((err) => {
+      if (err instanceof Error.ValidationError) {
+        next(new IncorrectDataError("Некорректные данные при создании пользователя"));
+      } else {
+        next(err);
+      }
+    });
 };
 
 export const updateCurrentUser = async (
--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,7 +3,7 @@ import { CustomRequest } from "../types/types";
 
 export default (req: CustomRequest, res: Response, next: NextFunction) => {
   req.user = {
-    _id: "648ac555cc68563f955b5d28"
+    _id: "648b7875d6b019b07577e826"
   };
   next();
 };
