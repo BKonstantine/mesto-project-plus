8e182189cbe67f38873d34876c2f9ff7a5c77105
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-06-17 16:13:59 +0300

    feat: Добавить поля email и password в модель user. Изменить поля name, about, avatar

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,18 +4,18 @@ export interface User {
   name: string;
   about: string;
   avatar: string;
+  email: string;
+  password: string;
 }
 
 const userSchema = new Schema<User>({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 200,
   },
@@ -23,11 +23,26 @@ const userSchema = new Schema<User>({
     type: String,
     validate: {
       validator: (v: any) => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+      },
+      message: "Некорректный формат ссылки",
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v: any) => {
         return /^(https?:\/\/)(www.)?[^\s]+(#?)$/i.test(v);
       },
       message: "Некорректный формат ссылки",
     },
+    unique: true,
+  },
+  password: {
+    type: String,
     required: true,
+    minlength: 8,
   },
 });
 
